dc7316a1a51fa44e9afc07e44accf6d5acd366c9
ClaudioBzrr <39713988+ClaudioBzrr@users.noreply.github.com>
2020-05-07 13:54:54 -0300

    slider criado e configurado.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,26 +1,24 @@
 import React from 'react'
-import './styles.css'
 import Header from '../../components/Header'
 import scene01 from '../../assets/scene01.PNG'
 import scene02 from '../../assets/scene02.PNG'
 import scene03 from '../../assets/scene03.PNG'
+import Awesomeslider from 'react-awesome-slider'
+import 'react-awesome-slider/dist/styles.css';
+import './styles.css'
+
+
 
 export default function About(){
     return(
-        <div>
+        <div className="main">
             <Header/>
             <div className="content">
-                <ul class="slider">
-                    <li>
-                        <img src={scene01} />
-                    </li>
-                    <li>
-                        <img src={scene02} />
-                    </li>
-                    <li>
-                        <img src={scene03} />
-                    </li>
-                </ul>
+                <Awesomeslider>
+                    <div className="sliders" data-src={scene01}/>
+                    <div className="sliders" data-src={scene02}/>
+                    <div className="sliders" data-src={scene03}/>
+                </Awesomeslider>
             </div>
             
         </div>
