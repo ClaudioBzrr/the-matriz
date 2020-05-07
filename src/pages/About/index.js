81922d4ce11052a3d3f5b61045db69cc7297c629
ClaudioBzrr <39713988+ClaudioBzrr@users.noreply.github.com>
2020-05-07 16:04:46 -0300

    Finalização do layout e personalização

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -4,10 +4,15 @@ import Header from '../../components/Header'
 
 export default function About(){
     return(
-        <div className="sobre">
+        <div className="sobreDiv">
             <Header/>
-            <section>
-                
+            <section className="grid">
+                <div className="bio">
+                    <h1 className="autores">Autores</h1>
+                    <div className="autor">Antonio Walter Alves de Souza</div>
+                    <div className="autor">Francisco Claudio Bezerra Dantas</div>
+                    <div className="git"><a className="repo"target="_blank" rel="noopener noreferrer" href="https://github.com/ClaudioBzrr/the-matriz.git">Visite o Repositório</a></div>
+                </div>
             </section>
             
         </div>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,23 +1,38 @@
 import React from 'react'
+import loading from '../../assets/now-loading.gif'
 import Header from '../../components/Header'
 import scene01 from '../../assets/scene01.jpg'
 import scene02 from '../../assets/scene02.jpg'
 import scene03 from '../../assets/scene03.jpg'
+import scene04 from '../../assets/scene04.JPG'
+import fim from '../../assets/fim.JPG'
 import Awesomeslider from 'react-awesome-slider'
-import 'react-awesome-slider/dist/styles.css';
+import 'react-awesome-slider/dist/styles.css'
 import './styles.css'
 
 
+function load(){
+    let load = document.querySelector('.load')
+    load.style.display = 'block'
+}
+
+window.addEventListener('DomContentLoaded',function(){
+    load()
+})
 
 export default function About(){
     return(
+        
         <div className="main">
+            <div className="load"> <img src={loading} alt="" srcset=""/></div>
             <Header/>
             <div className="content">
                 <Awesomeslider>
                     <div className="sliders" data-src={scene01}/>
                     <div className="sliders" data-src={scene02}/>
                     <div className="sliders" data-src={scene03}/>
+                    <div className="sliders" data-src={scene04}/>
+                    <div className="sliders" data-src={fim}/>
                 </Awesomeslider>
             </div>
             
